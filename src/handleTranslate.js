db4e6604068c6e422d7b948004a2971bfa67c600
Kyrua <ang02aezs@gmail.com>
2024-02-08 18:42:47 -0600

    languages ready

--- src/handleTranslate.js
+++ src/handleTranslate.js
@@ -6,8 +6,10 @@ const translator = new deepl.Translator(apiKey)
 
 async function handleTranslatorDeepl ({ text, fromLanguage, toLanguage }) {
     console.log(text, fromLanguage, toLanguage)
+    let newFromLanguage = fromLanguage?.includes("en") ? "en" : fromLanguage
+    console.log(newFromLanguage)
     return translator
-   .translateText( text, fromLanguage, toLanguage )
+   .translateText( text, newFromLanguage, toLanguage )
    .then((result) => {
        return (result);
    })
